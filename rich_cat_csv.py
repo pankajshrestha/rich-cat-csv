f42842d881a8898a411a532ad881b9d4d7d3e5f5
pankajshrestha <pankajshrestha@gmail.com>
2021-10-27 10:21:45 -0500

    Create rich_cat_csv.py

---
+++ rich_cat_csv.py
@@ -0,0 +1,26 @@
+import argparse
+import pandas as pd
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+table = Table(show_header=True, header_style="bold magenta")
+
+parser = argparse.ArgumentParser(description='Cat CSV file with Rich')
+parser.add_argument('--log_level', default='INFO')
+parser.add_argument('--csv_file_path', required=True)
+
+args = parser.parse_args()
+csv_file_path = args.csv_file_path
+
+df = pd.read_csv(csv_file_path, index_col=0)
+for col in df.columns:
+    table.add_column(col)
+
+
+for row in df.itertuples(index=False, name=None):
+    row = [str(item) for item in row]
+    table.add_row(*list(row))
+
+console.print(table)
